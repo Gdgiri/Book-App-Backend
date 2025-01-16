fbed6b3f82c586708fee711ff1fcfb0456547307
giridharan <giridharan831@gmail.com>
2025-01-16 17:13:47 +0530

    update

--- Controllers/ratingController.js
+++ Controllers/ratingController.js
@@ -11,53 +11,14 @@ export const addRating = async (req, res) => {
       return res.status(404).json({ message: "Story not found" });
     }
 
-    const existingRating = await Rating.findOne({
-      user: req.user._id,
-      story: storyId,
-    });
-
-    if (existingRating) {
-      return res
-        .status(400)
-        .json({ message: "You have already rated this story." });
-    }
-
     const newRating = await Rating.create({
-      user: req.user._id,
+      user: req.user._id, // Assumes you are passing authenticated user
       story: storyId,
       rating,
       comment,
     });
 
     res.status(201).json(newRating);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-// Get all ratings for a story
-export const getStoryRatings = async (req, res) => {
-  const { storyId } = req.params;
-
-  try {
-    const ratings = await Rating.find({ story: storyId }).populate(
-      "user",
-      "username email"
-    );
-    res.status(200).json(ratings);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-// Get user's ratings
-export const getUserRatings = async (req, res) => {
-  try {
-    const ratings = await Rating.find({ user: req.user._id }).populate(
-      "story",
-      "title description"
-    );
-    res.status(200).json(ratings);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- Routers/ratingRouter.js
+++ Routers/ratingRouter.js
@@ -1,15 +1,9 @@
 import express from "express";
 import { protect } from "../Middleware/authMiddleware.js";
-import {
-  addRating,
-  getStoryRatings,
-  getUserRatings,
-} from "../Controllers/ratingController.js";
+import { addRating } from "../Controllers/ratingController.js";
 
 const router = express.Router();
 
 router.post("/createrating", protect, addRating);
-router.get("/getratings/:storyId", protect, getStoryRatings);
-router.get("/getuserratings", protect, getUserRatings);
 
 export default router;

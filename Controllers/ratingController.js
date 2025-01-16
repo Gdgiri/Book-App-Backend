ddecebb3d04bd6f80f4a81285a6e167b93be8e09
giridharan <giridharan831@gmail.com>
2025-01-16 17:44:03 +0530

    update

--- Controllers/ratingController.js
+++ Controllers/ratingController.js
@@ -23,3 +23,17 @@ export const addRating = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getRatingsById = async (req, res) => {
+  try {
+    const { storyId } = req.params;
+
+    const reviews = await Rating.find({ story: storyId })
+      .populate("user", "username") // Populate user details (e.g., username)
+      .sort({ createdAt: -1 }); // Sort by latest reviews
+
+    res.status(200).json(reviews);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch reviews" });
+  }
+};
--- Routers/ratingRouter.js
+++ Routers/ratingRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protect } from "../Middleware/authMiddleware.js";
-import { addRating } from "../Controllers/ratingController.js";
+import { addRating, getRatingsById } from "../Controllers/ratingController.js";
 
 const router = express.Router();
 
 router.post("/createrating", protect, addRating);
+router.get("/reviews/:storyId", getRatingsById);
 
 export default router;
